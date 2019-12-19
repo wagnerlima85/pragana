7b01533fe62dc478b5f31948e4bd29a917e831ad
Wagner Lima <wagnerlima@cers.com.br>
2019-12-19 11:27:13 -0300

    Ajustando para umbler

--- server.js
+++ server.js
@@ -1,26 +1,14 @@
-var express = require('express');
-var path = require('path');
-var bodyParser = require('body-parser');
-var compression = require('compression');
-var app = express();
-
-app.use(compression());
-
-app.set('port', (process.env.PORT || 8082));
-
-app.use('/', express.static(__dirname + '/dist'));
-
-app.use(bodyParser.json());
-
-app.use(bodyParser.urlencoded({ extended: false }));
-
-  // all other routes are handled by Angular
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname,'/dist/index.html'));
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Angular 2 Full Stack listening on port '+app.get('port'));
-});
-
-module.exports = app;
+const express = require('express')
+const path = require('path')
+const port = process.env.PORT || 3000
+const app = express()
+ 
+app.use(express.static(__dirname))
+app.use(express.static(path.join(__dirname, 'dist', ''
+)))
+ 
+app.get('/', function (req, res) {
+   res.sendFile(path.join(__dirname, 'dist', '', 'index.html'))
+})
+ 
+app.listen(port)
